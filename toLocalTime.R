9939bfe0e324dfa8f758bc91a0b409a012af3c29
Chengcheng Qu <cqu@win.su.se>
2018-08-16 17:09:31 +0200

    Function to transform UTC offset time to local time

---
+++ toLocalTime.R
@@ -0,0 +1,19 @@
+# Identify time zone with UTC offset character
+# Adjust different time to local time
+require(stringi)
+require(lubridate)
+
+toLocalTime <- function(Date.Time){
+  # Input Date.Time format as "yyyy-mm-ddThh:mm:OSz"
+  op <- options(digits.secs=6) # Set format of fractional second
+  localDateTime <- with_tz(parse_date_time2(Date.Time, "YmdHMOSz"), tzone = "")
+  Date <- substr(localDateTime,1,10)
+  Time <- substr(localDateTime,12,27)
+  return(data.frame("Date" = Date, "Time" = Time))
+}
+
+# Example
+x <- c("2014-11-07T14:12:03.9917315143+01","2014-11-07T14:12:03.9917315143Z")
+toLocalTime(x)$Date
+toLocalTime(x)$Time
+
