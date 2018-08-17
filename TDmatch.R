73dc5b8dca13988bc0095046a4d53d5289d9eb3d
Chengcheng Qu <cqu@win.su.se>
2018-08-17 16:31:11 +0200

    Embed isDiffSession function to detect matching cross trading session

--- TDmatch.R
+++ TDmatch.R
@@ -3,6 +3,8 @@ require(ff)
 
 # Function to compress gz file
 source("gzcp-GZCompress.R")
+source("loadTrades.R")
+source("closestTradeDepth.R")
 
 # Function to get nth unique minimum -----------------------
 min.n <- function(x,n) {
@@ -11,96 +13,6 @@ min.n <- function(x,n) {
     return(tail(x,1))
   } else {
     return(sort(x, decreasing = FALSE)[n])
-  }
-}
-
-# Function to load data and set time -----------------------
-# Function to load data
-loadTrades <- function(fileDir){
-  # Load data with fread, requiring to add 7 Zip to the system path
-  comTrade <- fread(input = paste0("7z x -so ", fileDir), header = TRUE, check.names=T)
-  
-  # Split Date and Time from local exchange time, then create the variable Second
-  comTrade <- comTrade[, Date := toLocalTime(Date.Time)$Date]
-  comTrade <- comTrade[, Time := toLocalTime(Date.Time)$Time]
-  comTrade <- comTrade[order(comTrade$Date,comTrade$Time)]
-    
-  comTrade$Second <- strptime(comTrade$Time, format = "%H:%M:%OS", tz = "")$hour*3600+
-    strptime(comTrade$Time, format = "%H:%M:%OS", tz = "")$min*60+
-    strptime(comTrade$Time, format = "%H:%M:%OS", tz = "")$sec
-  
-  # Create name to 10-level depth
-  L10Depths <- c("L1.BidPrice", "L1.BidSize", "L1.AskPrice", "L1.AskSize",
-                 "L2.BidPrice", "L2.BidSize", "L2.AskPrice", "L2.AskSize",
-                 "L3.BidPrice", "L3.BidSize", "L3.AskPrice", "L3.AskSize",
-                 "L4.BidPrice", "L4.BidSize", "L4.AskPrice", "L4.AskSize",
-                 "L5.BidPrice", "L5.BidSize", "L5.AskPrice", "L5.AskSize",
-                 "L6.BidPrice", "L6.BidSize", "L6.AskPrice", "L6.AskSize",
-                 "L7.BidPrice", "L7.BidSize", "L7.AskPrice", "L7.AskSize",
-                 "L8.BidPrice", "L8.BidSize", "L8.AskPrice", "L8.AskSize",
-                 "L9.BidPrice", "L9.BidSize", "L9.AskPrice", "L9.AskSize",
-                 "L10.BidPrice", "L10.BidSize", "L10.AskPrice", "L10.AskSize")
-  
-  # Drop redundant variables for TAQ data
-  if(grepl("TAQ", fileDir)) {
-    # Delete delayed delivery
-    comTrade <- comTrade[!grepl("221\\[IRGCOND\\]", Qualifiers) & !grepl("230\\[IRGCOND\\]", Qualifiers), .(X.RIC, Type, Price, Volume, Date, Time, Second)]
-    comTrade <- comTrade[Price!=0 & !is.na(Price),]
-    
-    # Special treatment to half-day trading
-    halfTradeDay <- read.table("Z:/Documents/Futures rollover/Half-trading days.txt", header = TRUE)
-    halfTradeDay$Date <- as.character(halfTradeDay$Date)
-    
-    # Drop trades outside pre-trading, continuous trading, and post-trading sessions
-    comTrade <- comTrade[comTrade$Type=="Trade",]
-    
-    comTrade <- comTrade[(!comTrade$Date %in% halfTradeDay$Date & ((comTrade$Second >= 8*3600+30*60 & comTrade$Second < 8*3600+55*60) # Pre-trading
-                                                                   | (comTrade$Second >= 9*3600 & comTrade$Second < 17*3600+25*60) # Continuous trading
-                                                                   | (comTrade$Second >= 17*3600+30*60 & comTrade$Second < 18*3600))) # Post-trading
-                         | (comTrade$Date %in% halfTradeDay$Date & (comTrade$Second >= 8*3600+30*60 & comTrade$Second < 8*3600+55*60) # Pre-trading
-                            | (comTrade$Second >= 9*3600 & comTrade$Second < 12*3600+55*60) # Continuous trading
-                            | (comTrade$Second >= 13*3600 & comTrade$Second < 13*3600+30*60)), # Post-trading
-                         ]
-    
-    # Adding columns for depth data
-    # comTrade[,(L10Depths)] <- NULL 
-    comTrade[,c("bestBid", "bestAsk", "chgBid","chgAsk")] <- numeric(0)
-    comTrade[,(L10Depths)] <- numeric(0)
-  }
-  
-  # Drop redundant variables for Depth data
-  if(grepl("Depth", fileDir)) {
-    comTrade <- comTrade[which(!is.na(comTrade[, ..L10Depths])), 
-                         .(X.RIC, Type, Date, Time, Second,
-                             L1.BidPrice, L1.BidSize, L1.AskPrice, L1.AskSize,
-                             L2.BidPrice, L2.BidSize, L2.AskPrice, L2.AskSize,
-                             L3.BidPrice, L3.BidSize, L3.AskPrice, L3.AskSize,
-                             L4.BidPrice, L4.BidSize, L4.AskPrice, L4.AskSize,
-                             L5.BidPrice, L5.BidSize, L5.AskPrice, L5.AskSize,
-                             L6.BidPrice, L6.BidSize, L6.AskPrice, L6.AskSize,
-                             L7.BidPrice, L7.BidSize, L7.AskPrice, L7.AskSize,
-                             L8.BidPrice, L8.BidSize, L8.AskPrice, L8.AskSize,
-                             L9.BidPrice, L9.BidSize, L9.AskPrice, L9.AskSize,
-                             L10.BidPrice, L10.BidSize, L10.AskPrice, L10.AskSize)]
-    
-    # Chg. of depth as approx for trading volume
-    comTrade <- comTrade[, chgBid :=-diff(L1.BidSize, lag = 1, differences = 1), by=.(X.RIC, Date)]
-    comTrade <- comTrade[, chgAsk :=-diff(L1.AskSize, lag = 1, differences = 1), by=.(X.RIC, Date)]
-  }
-  return(comTrade)
-}
-
-# function to fix trading session----
-ifDiffSession <- function(tradeDate,tradeSec,depthSec){
-  # Half trading day according to the trading calendar
-  halfTradeDay <- read.table("Z:/Documents/Futures rollover/Half-trading days.txt", header = TRUE)
-  halfTradeDay$Date <- as.character(halfTradeDay$Date)
-  if (tradeDate %in% halfTradeDay$Date) {  
-    hourInterval <- c(8*3600+30*60, 8*3600+55*60, 9*3600, 12*3600+55*60, 13*3600, 13*3600+30*60)
-    return(findInterval(tradeSec, hourInterval)!= findInterval(depthSec, hourInterval))
-  } else {
-    hourInterval <- c(8*3600+30*60, 8*3600+55*60, 9*3600, 17*3600+25*60, 17*3600+30*60, 18*3600)
-    return(findInterval(tradeSec, hourInterval)!= findInterval(depthSec, hourInterval))
   }
 }
 
---
+++ cloesetTradeDepth.R
@@ -0,0 +1,49 @@
+# Function closestDepth 
+# and Function closestTrade
+
+require(data.table)
+
+# function to fix trading session----
+ifDiffSession <- function(tradeDate,tradeSec,depthSec){
+  # Half trading day according to the trading calendar
+  halfTradeDay <- read.table("Half-trading days.txt", header = TRUE)
+  halfTradeDay$Date <- as.character(halfTradeDay$Date)
+  if (tradeDate %in% halfTradeDay$Date) {  
+    hourInterval <- c(8*3600+30*60, 8*3600+55*60, 9*3600, 12*3600+55*60, 13*3600, 13*3600+30*60)
+    return(findInterval(tradeSec, hourInterval)!= findInterval(depthSec, hourInterval))
+  } else {
+    hourInterval <- c(8*3600+30*60, 8*3600+55*60, 9*3600, 17*3600+25*60, 17*3600+30*60, 18*3600)
+    return(findInterval(tradeSec, hourInterval)!= findInterval(depthSec, hourInterval))
+  }
+}
+
+# Function searching for closest matched depth --------
+# from all the depth records according to trade second
+closestDepth <- function(tradeSecond, depthSecond, tradeDate) {
+
+   if (tradeSecond>=min(depthSecond)) {
+    ind <- which.max((depthSecond-tradeSecond)[(depthSecond-tradeSecond)<=0]) # Find the index of the closest quote before
+    
+    if (ifDiffSession(tradeSecond, depthSecond[ind], tradeDate)) {
+      ind <- NA
+    }
+  }
+  
+  if (length(ind)>1) {
+    ind <- ind[1]
+    }
+  return(ind)
+}
+
+# Function searching for closest matched trade -----------------------
+closestTrade <- function(tradeSecond, trade2Second, tradeDate) {
+  
+    ind <- which.min(abs(trade2Second-tradeSecond)) # Find the index of the closest quote around: before or after
+    
+    if (ifDiffSession(tradeSecond, trade2Second[ind],tradeDate)) {
+      ind <- NA
+    }
+    
+    if (length(ind)>1) ind <- ind[1]
+  return(ind)
+}
